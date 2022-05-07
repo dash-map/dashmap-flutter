141d65d6f56d4985dae085d6eba951ededa8bcc1
together04 <pdh040124@gmail.com>
2022-05-07 13:17:30 +0900

    Add Login Page

--- lib/page/login.dart
+++ lib/page/login.dart
@@ -1,3 +1,5 @@
+import 'package:dash_map/page/home.dart';
+import 'package:dash_map/theme/color.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -6,7 +8,52 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/login_background.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 200,
+                height: 200,
+                child: Image.asset(
+                  'assets/login_logo.png',
+                  fit: BoxFit.contain,
+                ),
+              ),
+              const SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (BuildContext context) {
+                      return HomePage();
+                    }),
+                  );
+                },
+                child: const Text('login with github'),
+                style: ElevatedButton.styleFrom(
+                  primary: ColorData.black,
+                  textStyle: Theme.of(context)
+                      .textTheme
+                      .headline2
+                      ?.copyWith(color: ColorData.white),
+                  minimumSize: const Size(300, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(7.0),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
