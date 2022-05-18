96c1be9d3b58bab67f52d8bc097205817a43da42
together04 <pdh040124@gmail.com>
2022-05-18 16:23:24 +0900

    Add Roadmap Quest and Question

--- lib/page/roadmap/roadmap.dart
+++ lib/page/roadmap/roadmap.dart
@@ -1,3 +1,4 @@
+import 'package:dash_map/page/roadmap/roadmap_whole.dart';
 import 'package:dash_map/theme/color.dart';
 import 'package:dash_map/widget/app_bar.dart';
 import 'package:dash_map/widget/progress_bar.dart';
@@ -5,7 +6,9 @@ import 'package:dash_map/widget/speed_dial_button.dart';
 import 'package:flutter/material.dart';
 
 class RoadMapPage extends StatefulWidget {
-  const RoadMapPage({Key? key}) : super(key: key);
+  final String progress;
+
+  const RoadMapPage({Key? key, required this.progress}) : super(key: key);
 
   @override
   State<RoadMapPage> createState() => _RoadMapPageState();
@@ -17,68 +20,73 @@ class _RoadMapPageState extends State<RoadMapPage> {
     return Scaffold(
       appBar: myAppBar(),
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 12.0),
-        child: Center(
-          child: Column(
-            children: [
-              const SizedBox(height: 30.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('The Stack',
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodyText2
-                              ?.copyWith(color: ColorData.grey2)),
-                      const SizedBox(height: 5.0),
-                      Text('Frontend RoadMap',
-                          style: Theme.of(context).textTheme.headline1),
-                      const SizedBox(height: 5.0),
-                      OutlinedButton(
-                        child: Text(
-                          '로드맵 전체보기',
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodyText2
-                              ?.copyWith(color: ColorData.yellow),
-                        ),
-                        style: OutlinedButton.styleFrom(
-                          side: const BorderSide(
-                              width: 1.0, color: ColorData.yellow),
-                        ),
-                        onPressed: () {},
+        padding: const EdgeInsets.symmetric(horizontal: 32.0),
+        child: Column(
+          children: [
+            const SizedBox(height: 30.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('The Stack',
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyText2
+                            ?.copyWith(color: ColorData.grey2)),
+                    const SizedBox(height: 5.0),
+                    Text('Frontend RoadMap',
+                        style: Theme.of(context).textTheme.headline2),
+                    const SizedBox(height: 5.0),
+                    OutlinedButton(
+                      child: Text(
+                        '로드맵 전체보기',
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyText2
+                            ?.copyWith(color: ColorData.yellow),
                       ),
-                    ],
-                  ),
-                  SizedBox(
-                    width: 80,
-                    height: 80,
-                    child: Image.asset('assets/login_logo.png'),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 30.0),
-              RoadMapQuest(isTopic: false, question: '안녕', topic: 'HTTP'),
-              const SizedBox(height: 40.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  progressBar('40'),
-                  const SizedBox(width: 5.0),
-                  Text(
-                    '40%',
-                    style: Theme.of(context)
-                        .textTheme
-                        .bodyText1
-                        ?.copyWith(color: ColorData.orange2),
-                  )
-                ],
-              ),
-            ],
-          ),
+                      style: OutlinedButton.styleFrom(
+                        side: const BorderSide(
+                            width: 1.0, color: ColorData.yellow),
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (BuildContext context) => RoadMapWhole(),
+                          ),
+                        );
+                      },
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  width: 70,
+                  height: 70,
+                  child: Image.asset('assets/login_logo.png'),
+                ),
+              ],
+            ),
+            const SizedBox(height: 30.0),
+            RoadMapQuest(question: '안녕', topic: 'HTTP'),
+            const SizedBox(height: 40.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(child: progressBar(widget.progress)),
+                const SizedBox(width: 5.0),
+                Text(
+                  '${widget.progress}%',
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyText1
+                      ?.copyWith(color: ColorData.orange2),
+                )
+              ],
+            ),
+          ],
         ),
       ),
       floatingActionButton: speedDialButton(context),
@@ -87,13 +95,11 @@ class _RoadMapPageState extends State<RoadMapPage> {
 }
 
 class RoadMapQuest extends StatefulWidget {
-  final bool isTopic;
   final String question;
   final String topic;
 
   const RoadMapQuest({
     Key? key,
-    required this.isTopic,
     required this.question,
     required this.topic,
   }) : super(key: key);
@@ -103,50 +109,103 @@ class RoadMapQuest extends StatefulWidget {
 }
 
 class _RoadMapQuestState extends State<RoadMapQuest> {
+  late TextEditingController _myController;
+  bool isStudied = false;
+
+  @override
+  initState() {
+    _myController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _myController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       width: 280,
       height: 250,
+      padding: const EdgeInsets.all(12.0),
       decoration: BoxDecoration(
         border: Border.all(color: ColorData.black, width: 1),
         borderRadius: BorderRadius.circular(10.0),
       ),
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Visibility(
-            visible: widget.isTopic,
-            child: Text(
-              'Q. $widget.question',
-              style: Theme.of(context)
-                  .textTheme
-                  .headline1
-                  ?.copyWith(fontSize: 70, color: ColorData.grey1),
+          Flexible(
+            flex: 1,
+            child: Visibility(
+              visible: isStudied,
+              child: Text(
+                'Q. ${widget.question}',
+                style: Theme.of(context)
+                    .textTheme
+                    .headline2
+                    ?.copyWith(color: ColorData.grey1),
+                maxLines: null,
+              ),
             ),
           ),
-          Text(
-            widget.topic,
-            style: Theme.of(context)
-                .textTheme
-                .headline1
-                ?.copyWith(fontSize: 70, color: ColorData.grey1),
+          Flexible(
+            flex: 1,
+            child: isStudied
+                ? Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: TextFormField(
+                      controller: _myController,
+                      decoration: InputDecoration(
+                        hintText: '답을 입력해주세요.',
+                        hintStyle: Theme.of(context)
+                            .textTheme
+                            .bodyText2
+                            ?.copyWith(color: ColorData.grey2),
+                        enabledBorder: const UnderlineInputBorder(
+                          borderSide: BorderSide(color: ColorData.yellow),
+                        ),
+                        focusedBorder: const UnderlineInputBorder(
+                          borderSide: BorderSide(color: ColorData.yellow),
+                        ),
+                      ),
+                    ),
+                  )
+                : Text(
+                    widget.topic,
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline1
+                        ?.copyWith(fontSize: 70, color: ColorData.grey1),
+                  ),
           ),
-          ElevatedButton(
-            onPressed: () {
-              setState(() {
-                !widget.isTopic;
-              });
-            },
-            child: Text(
-              '완료',
-              style: Theme.of(context)
-                  .textTheme
-                  .bodyText2
-                  ?.copyWith(color: ColorData.white),
-            ),
-            style: ElevatedButton.styleFrom(
-              primary: ColorData.yellow,
+          Flexible(
+            flex: 1,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const SizedBox(),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      isStudied = !isStudied;
+                    });
+                  },
+                  child: Text(
+                    isStudied ? '완료' : '다음',
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyText2
+                        ?.copyWith(color: ColorData.white),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    primary: ColorData.yellow,
+                  ),
+                ),
+              ],
             ),
           ),
         ],
